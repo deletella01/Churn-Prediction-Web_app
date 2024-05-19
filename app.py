8a979534ab3c42fd1b73548ab22dd4bebb9ca1df
Bamidele Tella <60831529+deletella01@users.noreply.github.com>
2024-05-19 23:28:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-#from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 import warnings
 warnings.filterwarnings('ignore')
 
